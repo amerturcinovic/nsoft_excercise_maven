ea29d561fcf25969e98295da02713ad7c091f94a
Amer Turcinovic <amer.turcinovic@outlook.com>
2025-03-24 17:47:55 +0100

    init maven project and git

---
+++ fwcscoreboardlib/src/main/java/org/nsoft/exercise/scoreboard/App.java
@@ -0,0 +1,10 @@
+package org.nsoft.exercise.scoreboard;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
